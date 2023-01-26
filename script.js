63d859f415cfdadcae57657cc115e475fcda79de
Akashgupta19971998 <akashguptaakash2@gmail.com>
2023-01-26 21:04:55 +0530

    commit

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here
+let input=document.querySelector('input')
+let button=document.querySelector('btn')
+let para=document.querySelector('num')
 
+button.addEventListener('onclick', hotorcold)
+
+function hotorcold(){
+let input=randomNumber.value
+if(randomNumber<10){
+    para.innerHTML="cold";
+}
+else{
+    para.innerHTML="hot";
+}
+
+function randomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+}
+randomNumber(-20,20)
+
+}
